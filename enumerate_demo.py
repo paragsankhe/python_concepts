a3eb3c0c01b16df9833b0292fbf14899e55c7feb
“parag <psankhe777@gmail.com>
2025-04-02 23:08:23 +0530

    frst commit

---
+++ enumerate_demo.py
@@ -0,0 +1,20 @@
+
+# enumerate() function on list
+
+# fruits_list = ["apple","banana","cherry"]
+#
+# fruits_enum = enumerate(fruits_list)
+
+# for index,fruit in fruits_enum:
+#     print(index,fruit)
+
+# enumerate() function on string
+
+mystring = "hello"
+
+mystring_enum = enumerate(mystring)
+
+# for index,char in mystring_enum:
+#     print(index,char)
+
+print(list(mystring_enum))
